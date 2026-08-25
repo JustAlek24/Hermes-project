5025efe2290d9c7a7dc9a1897a7cab696fc8c4ab
Boxovod <sanyaablaev@gmail.com>
2026-08-25 18:12:25 +0300

    Убрал грёбанные лишние строки

--- src/network/connection.py
+++ src/network/connection.py
@@ -2,11 +2,7 @@ import asyncio
 import json
 import logging
 
-<<<<<<< HEAD
 from protocol.handler import handle_message
-=======
-from src.protocol import handle_message
->>>>>>> 8e0d8dd (Создал update_last_seen и немного изменил логику проверок)
 
 _connections = {}
 _connection_lock = asyncio.Lock()
