2df3f7dcbdf99e6abac2786d2397db5232dc09ae
Boxovod <sanyaablaev@gmail.com>
2026-08-30 02:26:12 +0300

    Сделал его(получение статусов было перенесено в core, так как из hearbeat не имеется возможность получить доступ, крч в коре всё хранится)

---
+++ src/network/heartbeat.py
@@ -0,0 +1,48 @@
+import asyncio
+import json
+from src.network.connection import connect_to_peer, send_message, receive_message
+from src.data.database import update_last_seen
+from src.protocol.messages import create_heartbeat
+
+async def heartbeat_loop(app, interval=60):
+
+    while True:
+        peers = app.get_all_peers(app.db)
+
+        if not peers:
+            print("Нет пиров, пропускам цикл")
+            await asyncio.sleep(interval)
+            continue
+
+        for peer in peers:
+            peer_id = peer["peer_id"]
+            ip = peer["ip"]
+            port = peer["port"]
+
+            try:
+                reader, writer = await connect_to_peer(ip, port)
+                if reader is None or writer is None:
+                    app.update_peer_status(peer_id, "offline")
+                    continue
+
+                heartbeat_dict = json.loads(create_heartbeat(app.my_peer_id))
+                success = await send_message(writer, heartbeat_dict)
+
+                if not success:
+                    app.update_peer_status = "offline"
+                    writer.close()
+                    continue
+
+                response = await receive_message(reader, timeout=5)
+                if response:
+                    app.update_peer_status(peer_id, "online")
+                    update_last_seen(app.db, peer_id)
+                else:
+                    app.update_peer_status(peer_id, "offline")
+
+                writer.close
+                await writer.wait_closed()
+
+            except Exception:
+                app.update_peer_status(peer_id, "offline")
+        await asyncio.sleep(interval)    
